ae0a540df68047819b949e99ad0d96155344e6bd
Adam Schubert <adam.schubert@sg1-game.net>
2015-06-18 21:36:23 +0200

    Add some info into header

--- index.php
+++ index.php
@@ -129,8 +129,9 @@ if (isset($_POST) && array_key_exists('obfuscated', $_POST))
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Javascript DE-Obfuscator (of javascriptobfuscator.com)">
+    <meta name="author" content="Adam Schubert">
+    <meta name="keywords" content="deobfuscator, Javascript, javascriptobfuscator">
 
     <title>Bare - Start Bootstrap Template</title>
 
